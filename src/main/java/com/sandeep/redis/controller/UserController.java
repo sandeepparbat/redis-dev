698db892720ac8077157aa735b469158cd3766ea
Sandeep Parbat <sandeep.parbat@gmail.com>
2020-03-30 19:31:25 +0530

    crud operation

--- src/main/java/com/sandeep/redis/controller/UserController.java
+++ src/main/java/com/sandeep/redis/controller/UserController.java
@@ -5,8 +5,13 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +32,19 @@ public class UserController {
 	public Optional<User> getUser(@PathVariable String userId) {
 	  LOG.info("Getting user with ID {}.", userId);
 	  return userRepository.findById(Long.valueOf(userId));
+	}
+	
+	@CachePut(value = "user", key = "#user.id")
+	@PutMapping("/update")
+	public User updatePersonByID(@RequestBody User user) {
+	  userRepository.save(user);
+	  return user;
+	}
+	
+	@CacheEvict(value = "user", allEntries=true)
+	@DeleteMapping("/{id}")
+	public void deleteUserByID(@PathVariable Long id) {
+	  LOG.info("deleting person with id {}", id);
+	  userRepository.deleteById(id);
 	}
 }
